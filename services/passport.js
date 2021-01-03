af5152061b71eb05bebdeedb70bfbdbaa231a638
Jintan09 <sfritsch09@gmail.com>
2021-01-03 18:48:24 +0100

    works fine now

--- services/passport.js
+++ services/passport.js
@@ -19,7 +19,7 @@ passport.use(
 		{
 			clientID: keys.googleClientID,
 			clientSecret: keys.googleClientSecret,
-			callbackURL: 'auth/google/callback',
+			callbackURL: '/auth/google/callback',
 			proxy: true,
 		},
 		async (accessToken, refreshToken, profile, done) => {
