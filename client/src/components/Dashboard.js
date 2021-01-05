7a9ed21e6ca299d410133cbffe5d676d0b4e80bf
Jintan09 <sfritsch09@gmail.com>
2021-01-05 18:16:13 +0100

    done

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import SurveyList from './surveys/SurveyList';
 
 const Dashboard = () => {
 	return (
 		<div>
-			Dashboard
+			<SurveyList />
 			<div className="fixed-action-btn">
-				<Link
-					style={{ position: 'absolute', bottom: '50px', right: '50px' }}
-					to="/surveys/new"
-					className="btn-floating btn-large red"
-				>
+				<Link to="/surveys/new" className="btn-floating btn-large red">
 					<i className="material-icons">add</i>
 				</Link>
 			</div>
--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Landing = () => {
-  return (
-    <div style={{ textAlign: 'center' }}>
-      <h1>
-        Emaily!
-      </h1>
-      Collect feedback form your users
-    </div>
-  );
+	return (
+		<div style={{ textAlign: 'center' }}>
+			<h1>Emaily!</h1>
+			Collect feedback form your users
+			<h2>
+				<Link to="/surveys">Let's Go</Link>
+			</h2>
+		</div>
+	);
 };
 
 export default Landing;
