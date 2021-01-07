f9eee7a99260056c6416f68a9311cc58257b5dd1
Jintan09 <sfritsch09@gmail.com>
2021-01-07 11:27:40 +0100

    update

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -8,6 +8,11 @@ const Landing = () => {
 			Collect feedback form your users
 			<h2>
 				<Link to="/surveys">Let's Go</Link>
+				<div className="test-warning">
+					*Please use the following test credit card for payments*
+					<br />
+					4242 4242 4242 4242 - Exp: 02/21 - CVV: 123
+				</div>
 			</h2>
 		</div>
 	);
